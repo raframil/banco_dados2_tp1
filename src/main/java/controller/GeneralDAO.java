5f19a5061e0caf030b6e4fc152d12a2aaea1db13
Rafael Greca <rafaelgreca@unifei.edu.br>
2019-09-12 13:10:13 -0300

    arquivos hibernate/dao

---
+++ src/main/java/controller/GeneralDAO.java
@@ -0,0 +1,54 @@
+//package controller;
+//
+//import java.io.Serializable;
+//import org.hibernate.*;
+//import Hibernate.HibernateUtil;
+//import java.util.ArrayList;
+//
+///**
+// *
+// * @author greca
+// */
+//public class GeneralDAO {
+//
+//    Session sessao;
+//
+//    public GeneralDAO() {
+//        sessao = (Session) HibernateUtil.getSessionFactory().openSession();
+//        
+//    }
+//    
+//    public Session getSessao() {
+//        return sessao;
+//    }
+//
+//    public void setSessao(Session sessao) {
+//        this.sessao = sessao;
+//    }
+//
+//
+//    public void salvar(Object entidade) {
+//        sessao.save(entidade);
+//    }
+//
+//    public Object carregar(Object entidade, Serializable id) {
+//        sessao.load(entidade, id);
+//        return entidade;
+//    }
+//
+//    public void apagar(Object entidade) {
+//        sessao.delete(entidade);
+//    }
+//
+//    public void atualizar(Object entidade) {
+//        sessao.update(entidade);
+//    }
+//
+//    public void fecharSessao() {
+//        sessao.close();
+//    }
+//
+//    public ArrayList<Object> listaTodos(String tabela) {
+//        return (ArrayList<Object>) sessao.createQuery("from "+tabela).list();
+//    }
+//}
